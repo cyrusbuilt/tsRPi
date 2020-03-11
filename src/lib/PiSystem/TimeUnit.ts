5cabbd7e67236892f76f9a247f8f36ddcbb056dc
cyrus <cyrusbuilt@gmail.com>
2020-03-11 15:30:19 -0400

    Adding TM16XX LED display support

---
+++ src/lib/PiSystem/TimeUnit.ts
@@ -0,0 +1,45 @@
+/**
+ * Time units of measure
+ * @enum
+ */
+export enum TimeUnit {
+  /**
+   * Time years.
+   */
+  YEARS = 1,
+
+  /**
+   * Time in months.
+   */
+  MONTHS = 2,
+
+  /**
+   * Time in weeks.
+   */
+  WEEKS = 3,
+
+  /**
+   * Time in days.
+   */
+  DAYS = 4,
+
+  /**
+   * Time in hours.
+   */
+  HOURS = 5,
+
+  /**
+   * Time in minutes.
+   */
+  MINUTES = 6,
+
+  /**
+   * Time in seconds.
+   */
+  SECONDS = 7,
+
+  /**
+   * Time in milliseconds.
+   */
+  MILLISECONDS = 8,
+}
